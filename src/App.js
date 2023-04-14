34a91153cfd53270f9886db87d2f75b81fa928fe
loknathsd <loknathsds66@gmail.com>
2023-04-14 22:34:25 +0600

    routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./components/Topbar/Topbar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -14,6 +16,9 @@ function App() {
         <div className="app">
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
