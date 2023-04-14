70c73260a8f7e6879f1ba72e8c56be596df83a57
loknathsd <loknathsds66@gmail.com>
2023-04-14 21:39:22 +0600

    navbar with dark mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./components/Topbar/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <ColorModeContext >
+    <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+          </main>
         </div>
       </ThemeProvider>
-    </ColorModeContext>
+    </ColorModeContext.Provider>
   );
 }
 
